7c989e5b0111c80d298581f5156b7bf1c0578e25
mehedi rana <mdmehedirana@gmail.com>
2020-06-12 05:50:16 +0600

    add into firebase

--- App.js
+++ App.js
@@ -9,23 +9,51 @@ firbase.initializeApp(firebaseConfig)
 
 export default class App extends React.Component {
 
+  state={
+    text: "",
+    myList:[],
+
+  }
+
+  componentDidMount(){
+    const onData = firbase.database().ref("items")
+    onData.on("value",dataShanp=>{
+      console.log(dataShanp.val())
+    })
+  }
+  saveData(){
+    const inData = firbase.database().ref("users")
+    inData.push().set({
+      text: this.state.text,
+      time: Date.now()
+    })
+
+  }
+
   render() {
 
     return (
       <View style={styles.container}>
         <Item floatingLabel>
           <Label>Enter Iteam</Label>
-          <Input />
+          <Input 
+           value={this.state.text}
+           onChangeText={(input)=>{this.setState({text:input})}}
+          />
         </Item>
         {/* <Item floatingLabel last>
         <Label>Password</Label>
         <Input />
       </Item> */}
         <View style={{ flexDirection: "row", padding: 20, justifyContent: 'space-between' }}>
-          <Button bordered success style={styles.mbtn}>
+          <Button bordered success style={styles.mbtn}
+          onPress={()=>{this.saveData()}}
+          >
             <Text>ADD</Text>
           </Button>
-          <Button bordered danger style={styles.mbtn}>
+          <Button bordered danger style={styles.mbtn}
+          
+          >
             <Text>DELETE</Text>
           </Button>
 
