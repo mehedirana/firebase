872694c0d26e1e6136bb5859c6196137f57aeb12
mehedi rana <mdmehedirana@gmail.com>
2020-06-11 07:32:43 +0600

    add nativebase

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Item, Input, Label, Button } from 'native-base';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Item floatingLabel>
+        <Label>Username</Label>
+        <Input />
+      </Item>
+      <Item floatingLabel last>
+        <Label>Password</Label>
+        <Input />
+      </Item>
+      <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+        <Button bordered success>
+          <Text>Success</Text>
+        </Button>
+        <Button bordered danger>
+          <Text>Danger</Text>
+        </Button>
+
+      </View>
     </View>
   );
 }
