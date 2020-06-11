5b9acfacb8c66befb6f56b85faf7c527eecb69d2
mehedi rana <mdmehedirana@gmail.com>
2020-06-12 04:57:25 +0600

    function to class com

--- App.js
+++ App.js
@@ -3,31 +3,36 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Item, Input, Label, Button } from 'native-base';
 import Constants from 'expo-constants';
 import * as firbase from 'firebase';
-import {firebaseConfig} from './config'
+import { firebaseConfig } from './config'
 
 firbase.initializeApp(firebaseConfig)
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Item floatingLabel>
-        <Label>Enter Iteam</Label>
-        <Input />
-      </Item>
-      {/* <Item floatingLabel last>
+
+export default class App extends React.Component {
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <Item floatingLabel>
+          <Label>Enter Iteam</Label>
+          <Input />
+        </Item>
+        {/* <Item floatingLabel last>
         <Label>Password</Label>
         <Input />
       </Item> */}
-      <View style={{ flexDirection: "row", padding: 20, justifyContent: 'space-between' }}>
-      <Button bordered success style={styles.mbtn}>
+        <View style={{ flexDirection: "row", padding: 20, justifyContent: 'space-between' }}>
+          <Button bordered success style={styles.mbtn}>
             <Text>ADD</Text>
           </Button>
           <Button bordered danger style={styles.mbtn}>
             <Text>DELETE</Text>
           </Button>
 
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
